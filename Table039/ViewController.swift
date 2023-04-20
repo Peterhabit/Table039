042e99cb0179a8456c22d8a299fc793382ed9695
kjw <lkk7209@naver.com>
2023-04-20 10:22:53 +0900

    Initial Commit

---
+++ Table039/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table039
+//
+//  Created by 소프트웨어컴퓨터 on 2023/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
